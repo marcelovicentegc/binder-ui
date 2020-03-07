a2036ea52cbd4aa66a721ef8e90b86ce84c55bfd
Valeria <root@DESKTOP-V51GC2H.localdomain>
2020-03-06 23:30:05 -0300

    changing layout

--- src/components/Steps/StepEllipse/style.ts
+++ src/components/Steps/StepEllipse/style.ts
@@ -4,7 +4,7 @@ export const StyledStepEllipse = styled.div<{ isCurrentStep: boolean }>`
   ${({ theme, isCurrentStep }) => `width: 7px;
   height: 7px;
   border: solid 2px #000000;
-  border: solid 2px ${theme.color.white1};
+  border: solid 2px ${theme.color.gray2};
   border-radius: 50%;
   margin: 0px 5px;
   cursor: pointer;
@@ -13,6 +13,7 @@ export const StyledStepEllipse = styled.div<{ isCurrentStep: boolean }>`
   ${isCurrentStep &&
     `background-color: #000000;
     background-color: ${theme.color.gray2};
+    border-radius: 32px;
     height: 8px;
     mix-blend-mode: difference;`}
     width: 16px;
